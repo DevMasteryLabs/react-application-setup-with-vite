222d9d616b07b654870a9d1cdf60def6115e108e
DevMasteryLabs <develop.devmastery@gmail.com>
2023-09-27 00:08:07 +0100

    Set up React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Posts from './pages/Posts'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/posts' element={<Posts />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
