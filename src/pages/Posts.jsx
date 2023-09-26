5846ca71f39822ce7145ddf1e5509ea6c721ea9f
DevMasteryLabs <develop.devmastery@gmail.com>
2023-09-27 00:25:46 +0100

    Retrieve data from an external API

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        setPosts(res.data.slice(0, 10))
+      })
+      .catch((err) => {
+        setError(err.message)
+      })
+  }, [])
+
   return (
     <div className='container mt-2'>
       <h3 className='text-center'>Posts</h3>
